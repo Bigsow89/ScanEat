f8fbfa5cc4c6999a5a72b7544c5cec0b85021537
unknown <aysamuel007@gmail.com>
2023-03-28 11:55:45 +0200

    homepage done

--- client/screens/Home.js
+++ client/screens/Home.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
 
 export default function Home() {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Image
+        source={require('../assets/empty-home-page.png')}
+        style={styles.image}
+      />
+      <Text style={styles.text}>Hey, wanna scan some items?</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: 240,
+    height: 240,
+    opacity: 0.7,
+  },
+  text: {
+    fontSize: 24,
+    paddingTop: 20,
+    opacity: 0.5,
+  },
+});
