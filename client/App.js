5156fe1e7b99302b84932b75ccdb96411574539e
unknown <aysamuel007@gmail.com>
2023-03-30 18:09:14 +0200

    allformsdonenotyetvalidated

--- client/App.js
+++ client/App.js
@@ -1,105 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView  } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Icon } from '@rneui/themed';
-import Home from './screens/Home';
-import Scanner from './screens/Scanner';
-import Search from './screens/Search';
-import Profile from './screens/Profile';
+import {
+  View,
+  Text,
+  StyleSheet,
+  StatusBar,
+  KeyboardAvoidingView,
+} from 'react-native';
+import React from 'react';
+import 'react-native-gesture-handler';
+import AllTabs from './screens/AllTabs';
+import LoginScreen from './screens/LoginScreen/LoginScreen';
+import Navigation from './screens/Navigation';
 
-const Tab = createBottomTabNavigator();
-
-export default function App() {
+const App = () => {
   return (
-    <SafeAreaView style={styles.AndroidSafeArea}>
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={{
-          tabBarShowLabel: false,
-          tabBarActiveTintColor: '#c9d7ae',
-          tabBarHideOnKeyboard: true,
-          tabBarInactiveTintColor: '#ffffff',
-          tabBarStyle: {
-            backgroundColor: '#0c8079',
-            height: 60,
-          },
-        }}>
-        <Tab.Screen
-          name='Home'
-          component={Home}
-          options={{
-            headerShown: false,
-            tabBarIcon: (props) => (
-              <Icon
-                name='home'
-                type='feather'
-                color={props.color}
-              />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name='Scanner'
-          component={Scanner}
-          options={{
-            headerShown: false,
-            tabBarIcon: (props) => (
-              <Icon
-                name='maximize'
-                type='feather'
-                color={props.color}
-              />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name='Search'
-          component={Search}
-          options={{
-            headerShown: false,
-            tabBarIcon: (props) => (
-              <Icon
-                name='search'
-                type='feather'
-                color={props.color}
-              />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name='Profile'
-          component={Profile}
-          options={{
-            headerShown: false,
-            tabBarIcon: (props) => (
-              <Icon
-                name='user'
-                type='feather'
-                color={props.color}
-              />
-            ),
-          }}
-        />
-      </Tab.Navigator>
-      <StatusBar style='auto' />
-    </NavigationContainer>
-    </SafeAreaView>
+    <View style={styles.root}>
+      {/* <LoginScreen /> */}
+      {/* <AllTabs /> */}
+      <Navigation />
+    </View>
   );
-}
+};
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
 const styles = StyleSheet.create({
-  AndroidSafeArea: {
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-    flex: 1, 
-    justifyContent: "flex-end"
-  }
+  root: {
+    flex: 1,
+    // paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    backgroundColor: '#eef2e6',
+  },
 });
+
+export default App;
