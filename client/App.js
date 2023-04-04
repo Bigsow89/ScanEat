89c2dcdf8c996af064ebfff85c121222ae1ff475
Sada Sow <sadasow@Sadas-M1-MacBook-Pro.local>
2023-04-04 16:34:28 +0200

    product details

--- client/App.js
+++ client/App.js
@@ -1,24 +1,24 @@
-
-import { AppRegistry, StyleSheet } from 'react-native';
-import React from 'react';
+import { AppRegistry, StyleSheet } from "react-native";
+import React from "react";
 //import 'react-native-gesture-handler';
-import {name as appName} from './app.json';
+import { name as appName } from "./app.json";
 import { registerRootComponent } from "expo";
-import { NavigationContainer } from '@react-navigation/native';
-import MainNavigator from './components/MainNavigator';
-import LoginProvider from './Context/LoginProvider'
+import { NavigationContainer } from "@react-navigation/native";
+import MainNavigator from "./components/MainNavigator";
+import LoginProvider from "./Context/LoginProvider";
+import ProductDetail from "./components/ProductDetail";
 const App = () => {
   //const isLoggedIn = false;
- 
-  return (
-  <>
-    <LoginProvider>
-    <NavigationContainer>
-      <MainNavigator />
-    </NavigationContainer>
-  </LoginProvider>
-  </>
 
+  return (
+    <>
+      <LoginProvider>
+        <NavigationContainer>
+          <MainNavigator />
+        </NavigationContainer>
+      </LoginProvider>
+      {/* <ProductDetail /> */}
+    </>
   );
 };
 export default App;
